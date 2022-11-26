1f75db03e72a1fdc9635739abb6a41a95f04dd4a
hins <277439189@qq.com>
2022-11-26 15:39:50 +0800

    support native password in DBConifg

--- framework/contract/orm.go
+++ framework/contract/orm.go
@@ -23,21 +23,22 @@ type DBOption func(container framework.Container, config *DBConfig) error
 // DBConfig 代表数据库连接的所有配置
 type DBConfig struct {
 	// 以下配置关于dsn
-	WriteTimeout string `yaml:"write_timeout"` // 写超时时间
-	Loc          string `yaml:"loc"`           // 时区
-	Port         int    `yaml:"port"`          // 端口
-	ReadTimeout  string `yaml:"read_timeout"`  // 读超时时间
-	Charset      string `yaml:"charset"`       // 字符集
-	ParseTime    bool   `yaml:"parse_time"`    // 是否解析时间
-	Protocol     string `yaml:"protocol"`      // 传输协议
-	Dsn          string `yaml:"dsn"`           // 直接传递dsn，如果传递了，其他关于dsn的配置均无效
-	Database     string `yaml:"database"`      // 数据库
-	Collation    string `yaml:"collation"`     // 字符序
-	Timeout      string `yaml:"timeout"`       // 连接超时时间
-	Username     string `yaml:"username"`      // 用户名
-	Password     string `yaml:"password"`      // 密码
-	Driver       string `yaml:"driver"`        // 驱动
-	Host         string `yaml:"host"`          // 数据库地址
+	WriteTimeout        string `yaml:"write_timeout"`         // 写超时时间
+	Loc                 string `yaml:"loc"`                   // 时区
+	Port                int    `yaml:"port"`                  // 端口
+	ReadTimeout         string `yaml:"read_timeout"`          // 读超时时间
+	Charset             string `yaml:"charset"`               // 字符集
+	ParseTime           bool   `yaml:"parse_time"`            // 是否解析时间
+	Protocol            string `yaml:"protocol"`              // 传输协议
+	Dsn                 string `yaml:"dsn"`                   // 直接传递dsn，如果传递了，其他关于dsn的配置均无效
+	Database            string `yaml:"database"`              // 数据库
+	Collation           string `yaml:"collation"`             // 字符序
+	Timeout             string `yaml:"timeout"`               // 连接超时时间
+	Username            string `yaml:"username"`              // 用户名
+	Password            string `yaml:"password"`              // 密码
+	Driver              string `yaml:"driver"`                // 驱动
+	Host                string `yaml:"host"`                  // 数据库地址
+	AllowNativePassword bool   `yaml:"allow_native_password"` // 是否允许native passowrd 加密，mysql8默认不允许
 
 	// 以下配置关于连接池
 	ConnMaxIdle     int    `yaml:"conn_max_idle"`     // 最大空闲连接数
@@ -69,17 +70,18 @@ func (conf *DBConfig) FormatDsn() (string, error) {
 		return "", err
 	}
 	driverConf := &mysql.Config{
-		User:         conf.Username,
-		Passwd:       conf.Password,
-		Net:          conf.Protocol,
-		Addr:         net.JoinHostPort(conf.Host, port),
-		DBName:       conf.Database,
-		Collation:    conf.Collation,
-		Loc:          location,
-		Timeout:      timeout,
-		ReadTimeout:  readTimeout,
-		WriteTimeout: writeTimeout,
-		ParseTime:    conf.ParseTime,
+		User:                 conf.Username,
+		Passwd:               conf.Password,
+		Net:                  conf.Protocol,
+		Addr:                 net.JoinHostPort(conf.Host, port),
+		DBName:               conf.Database,
+		Collation:            conf.Collation,
+		Loc:                  location,
+		Timeout:              timeout,
+		ReadTimeout:          readTimeout,
+		WriteTimeout:         writeTimeout,
+		ParseTime:            conf.ParseTime,
+		AllowNativePasswords: conf.AllowNativePassword,
 	}
 	return driverConf.FormatDSN(), nil
 }
